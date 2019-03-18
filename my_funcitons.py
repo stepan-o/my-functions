202734989ebcdbf1f64dd43e6b17d7feac5832eb
funduck007 <stepanoskin007@gmail.com>
2019-03-18 00:45:15 -0400

    added 'string_concat'

--- my_funcitons.py
+++ my_funcitons.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from collections.abc import Iterable
 from gensim.models.doc2vec import LabeledSentence
+from gensim.models.phrases import Phrases
+from gensim.models.phrases import Phraser
 
 
 def unique_values(df_column: pd.Series, value_counts=True):
@@ -196,6 +198,33 @@ def labelize_tweets_bg(token_series, tweets, label):
         result.append(LabeledSentence(bigram[t],
                                       [prefix + '_%s' % i]))
     return result
+
+
+def string_concat(ser, string_name="", display_sym=500,
+                  input_type='strings'):
+    """
+    a function to concatenate a single string
+    out of a series of text documents
+    """
+    con_string = []
+
+    if input_type == 'strings':
+        for text in ser:
+            con_string.append(text)
+
+    elif input_type == 'lists':
+        for str_list in ser:
+            con_string.append(" ".join(str_list))
+    else:
+        print("'input_type' must be 'strings' or 'lists'.")
+
+    con_string = pd.Series(con_string).str.cat(sep=' ')
+
+    print("First {0} symbols in the {1} string:\n"
+          .format(display_sym, string_name))
+    print(con_string[:display_sym])
+
+    return con_string
 
 
 def plot_time_series(series_to_plot, summary_stats=False,
